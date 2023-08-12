9a55f04463501266ca75338506d809a071658e62
odeyalo <theprogrammerinfuture@gmail.com>
2023-08-12 20:50:23 +0300

    Configuration class that used to enable the Suite reactive module

---
+++ src/main/java/com/odeyalo/sonata/connect/config/SuiteReactiveConfiguration.java
@@ -0,0 +1,13 @@
+package com.odeyalo.sonata.connect.config;
+
+import com.odeyalo.sonata.suite.reactive.annotation.EnableSuiteReactive;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class that used to enable the Suite reactive module
+ */
+@Configuration
+@EnableSuiteReactive
+public class SuiteReactiveConfiguration {
+
+}
