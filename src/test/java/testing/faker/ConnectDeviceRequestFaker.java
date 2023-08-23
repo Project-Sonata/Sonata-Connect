2fc005fa8f6e4d88b183ac39c72387f809143b4b
odeyalo <theprogrammerinfuture@gmail.com>
2023-08-23 12:48:43 +0300

    Faker to create close-to-real data for ConnectDeviceRequest

---
+++ src/test/java/testing/faker/ConnectDeviceRequestFaker.java
@@ -0,0 +1,49 @@
+package testing.faker;
+
+import com.github.javafaker.Faker;
+import com.odeyalo.sonata.connect.dto.ConnectDeviceRequest;
+import com.odeyalo.sonata.connect.model.DeviceType;
+import lombok.AccessLevel;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.apache.commons.lang3.RandomStringUtils;
+
+/**
+ * Faker to create close-to-real data for {@link ConnectDeviceRequest}
+ */
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ConnectDeviceRequestFaker {
+    String deviceId;
+    String deviceName;
+    DeviceType deviceType;
+    int volume;
+
+    final Faker faker = Faker.instance();
+
+    protected ConnectDeviceRequestFaker() {
+        this.deviceId = RandomStringUtils.randomAlphanumeric(10);
+        this.deviceName = RandomStringUtils.randomAlphabetic(15);
+        this.deviceType = faker.options().option(DeviceType.class);
+        this.volume = faker.random().nextInt(0, 100);
+    }
+
+    public static ConnectDeviceRequestFaker create() {
+        return new ConnectDeviceRequestFaker();
+    }
+
+    public ConnectDeviceRequest get() {
+        return buildInMemoryDevice();
+    }
+
+
+    private ConnectDeviceRequest buildInMemoryDevice() {
+        return ConnectDeviceRequest
+                .builder()
+                .id(deviceId)
+                .name(deviceName)
+                .deviceType(deviceType)
+                .volume((byte) volume)
+                .build();
+    }
+}
