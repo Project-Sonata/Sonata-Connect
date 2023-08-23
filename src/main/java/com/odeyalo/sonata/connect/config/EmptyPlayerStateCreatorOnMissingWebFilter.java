3aba4d523ab902369952c32486df13240724f064
odeyalo <theprogrammerinfuture@gmail.com>
2023-08-23 12:46:43 +0300

    Web filter that used to create empty player state if it is missing for the user

---
+++ src/main/java/com/odeyalo/sonata/connect/config/EmptyPlayerStateCreatorOnMissingWebFilter.java
@@ -0,0 +1,34 @@
+package com.odeyalo.sonata.connect.config;
+
+import com.odeyalo.sonata.connect.model.User;
+import com.odeyalo.sonata.connect.service.player.BasicPlayerOperations;
+import com.odeyalo.suite.security.auth.AuthenticatedUser;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * Web filter that used to create empty player state if it is missing for the user
+ */
+@Component
+public class EmptyPlayerStateCreatorOnMissingWebFilter implements WebFilter {
+
+    final BasicPlayerOperations playerOperations;
+
+    public EmptyPlayerStateCreatorOnMissingWebFilter(BasicPlayerOperations playerOperations) {
+        this.playerOperations = playerOperations;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return ReactiveSecurityContextHolder.getContext().map(SecurityContext::getAuthentication)
+                .cast(AuthenticatedUser.class)
+                .map(user -> User.of(user.getDetails().getId()))
+                .flatMap(playerOperations::createState)
+                .flatMap(state -> chain.filter(exchange));
+    }
+}
