afa670237d0eb682feb526e118d058de42f0927b
odeyalo <theprogrammerinfuture@gmail.com>
2023-08-27 13:50:11 +0300

    Added docs for the interface

--- src/main/java/com/odeyalo/sonata/connect/entity/PlayerState.java
+++ src/main/java/com/odeyalo/sonata/connect/entity/PlayerState.java
@@ -3,24 +3,56 @@ package com.odeyalo.sonata.connect.entity;
 import com.odeyalo.sonata.connect.model.PlayingType;
 import com.odeyalo.sonata.connect.model.RepeatState;
 
+/**
+ * Entity to represent the current player state for the specfic user
+ */
 public interface PlayerState {
     boolean SHUFFLE_ENABLED = true;
     boolean SHUFFLE_DISABLED = false;
 
+    /**
+     * @return ID of the state
+     */
     Long getId();
 
+    /**
+     * @return repeat state for current player
+     */
     RepeatState getRepeatState();
 
+    /**
+     * @return shuffle state for the player, true - shuffle enabled, false - shuffle disabled
+     */
     boolean getShuffleState();
 
+    /**
+     * @return Progress of the track, episode, podcast, etc associated with current player
+     */
     Long getProgressMs();
 
+    /**
+     * @return the current playing type
+     */
     PlayingType getCurrentlyPlayingType();
 
+    /**
+     *
+     * @return item that is playing right now. Null if player state is empty
+     */
+    PlayableItemEntity getCurrentlyPlayingItem();
+
+    /**
+     * @return True if the player is playing something, false otherwise
+     */
     boolean isPlaying();
 
+    /**
+     * @return Devices associated with current player
+     */
     Devices getDevices();
 
+    /**
+     * @return Owner of the player
+     */
     UserEntity getUser();
-    PlayableItemEntity getCurrentlyPlayingItem();
 }
