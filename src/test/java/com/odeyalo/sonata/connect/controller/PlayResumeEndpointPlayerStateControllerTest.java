5c4fb3d116e3ba2098abc0d61dc80754c7e3d591
odeyalo <theprogrammerinfuture@gmail.com>
2023-08-29 22:11:46 +0300

    Written tests for /play endpoint

--- src/test/java/com/odeyalo/sonata/connect/controller/PlayResumeEndpointPlayerStateControllerTest.java
+++ src/test/java/com/odeyalo/sonata/connect/controller/PlayResumeEndpointPlayerStateControllerTest.java
@@ -1,7 +1,9 @@
 package com.odeyalo.sonata.connect.controller;
 
+import com.odeyalo.sonata.connect.dto.ConnectDeviceRequest;
 import com.odeyalo.sonata.connect.dto.PlayResumePlaybackRequest;
 import com.odeyalo.sonata.connect.dto.PlayerStateDto;
+import com.odeyalo.sonata.connect.dto.ReasonCodeAwareExceptionMessage;
 import com.odeyalo.sonata.connect.repository.storage.PlayerStateStorage;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.*;
@@ -15,6 +17,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Hooks;
 import testing.asserts.PlayerStateDtoAssert;
+import testing.asserts.ReasonCodeAwareExceptionMessageAssert;
+import testing.faker.ConnectDeviceRequestFaker;
 
 import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.REMOTE;
 
@@ -48,11 +52,65 @@ public class PlayResumeEndpointPlayerStateControllerTest {
 
     @Test
     void shouldReturn204StatusWithBody() {
+        connectDevice();
+
         String trackUri = "sonata:track:cassie";
 
         WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(trackUri));
 
         responseSpec.expectStatus().isNoContent();
+    }
+
+    @Test
+    void shouldChangePlayerCurrentPlayableItemToProvided() {
+        connectDevice();
+
+        String trackUri = "sonata:track:cassie";
+
+        WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(trackUri));
+
+        PlayerStateDto updatedState = getCurrentState();
+
+        PlayerStateDtoAssert.forState(updatedState)
+                .track().id().isEqualTo("cassie");
+    }
+
+    @Test
+    void shouldReturn400BadRequestIfContextUriMalformed() {
+        connectDevice();
+        String invalidContextUri = "sonata:somethinginvalid:mikuuuu";
+
+        WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(invalidContextUri));
+
+        responseSpec.expectStatus().isBadRequest();
+    }
+
+    @Test
+    void shouldReturnExceptionMessage() {
+        connectDevice();
+
+        String invalidContextUri = "sonata:somethinginvalid:mikuuuu";
+
+        WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(invalidContextUri));
+
+        ReasonCodeAwareExceptionMessage body = responseSpec.expectBody(ReasonCodeAwareExceptionMessage.class).returnResult().getResponseBody();
+
+        ReasonCodeAwareExceptionMessageAssert.forMessage(body)
+                .description().isEqualTo("Context uri is malformed");
+    }
+
+    @Test
+    void shouldReturnExceptionReasonCode() {
+        connectDevice();
+
+        String invalidContextUri = "sonata:somethinginvalid:mikuuuu";
+
+        WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(invalidContextUri));
+
+        ReasonCodeAwareExceptionMessage body = responseSpec.expectBody(ReasonCodeAwareExceptionMessage.class).returnResult().getResponseBody();
+
+        ReasonCodeAwareExceptionMessageAssert.forMessage(body)
+                .reasonCode().isEqualTo("malformed_context_uri");
     }
 
     @Test
@@ -72,6 +130,56 @@ public class PlayResumeEndpointPlayerStateControllerTest {
         WebTestClient.ResponseSpec responseSpec = sendResumeCurrentPlaybackEndpointRequest();
 
         responseSpec.expectStatus().isBadRequest();
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class EmptyDeviceListTests {
+
+        @Test
+        void shouldReturnBadRequest() {
+            String trackUri = "sonata:track:cassie";
+
+            WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(trackUri));
+
+            responseSpec.expectStatus().isBadRequest();
+        }
+
+        @Test
+        void shouldReturnExceptionMessageInBody() {
+            String trackUri = "sonata:track:cassie";
+
+            WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(trackUri));
+
+            ReasonCodeAwareExceptionMessage body = responseSpec.expectBody(ReasonCodeAwareExceptionMessage.class).returnResult().getResponseBody();
+
+            ReasonCodeAwareExceptionMessageAssert.forMessage(body)
+                    .description().isEqualTo("There is no active device");
+        }
+
+        @Test
+        void shouldReturnExceptionReasonInBody() {
+            String trackUri = "sonata:track:cassie";
+
+            WebTestClient.ResponseSpec responseSpec = sendResumeEndpointRequest(PlayResumePlaybackRequest.of(trackUri));
+
+            ReasonCodeAwareExceptionMessage body = responseSpec.expectBody(ReasonCodeAwareExceptionMessage.class).returnResult().getResponseBody();
+
+            ReasonCodeAwareExceptionMessageAssert.forMessage(body)
+                    .reasonCode().isEqualTo("no_active_device");
+        }
+    }
+
+    @NotNull
+    private ConnectDeviceRequest connectDevice() {
+        ConnectDeviceRequest connectDeviceRequest = ConnectDeviceRequestFaker.create().get();
+        WebTestClient.ResponseSpec response = webTestClient.put()
+                .uri("/player/device/connect")
+                .header(HttpHeaders.AUTHORIZATION, VALID_ACCESS_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(connectDeviceRequest)
+                .exchange();
+        return connectDeviceRequest;
     }
 
     @NotNull
