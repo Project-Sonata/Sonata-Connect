99f732bb4b621a44893e04296043789c47e9a4b6
odeyalo <theprogrammerinfuture@gmail.com>
2023-08-29 21:49:26 +0300

    Changed builder to SuperBuilder

--- src/main/java/com/odeyalo/sonata/connect/dto/ExceptionMessage.java
+++ src/main/java/com/odeyalo/sonata/connect/dto/ExceptionMessage.java
@@ -3,10 +3,11 @@ package com.odeyalo.sonata.connect.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor(staticName = "of")
 @NoArgsConstructor
-@Builder
+@SuperBuilder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 public class ExceptionMessage {
