4d709f35a3f3291b462cfa5b5f309d49edb92897
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-02 23:56:41 +0300

    Call endpoints using WebTestClient

---
+++ src/test/java/testing/shared/WebTestClientSonataTestHttpOperations.java
@@ -0,0 +1,69 @@
+package testing.shared;
+
+import com.odeyalo.sonata.connect.dto.AvailableDevicesResponseDto;
+import com.odeyalo.sonata.connect.dto.ConnectDeviceRequest;
+import com.odeyalo.sonata.connect.dto.PlayResumePlaybackRequest;
+import com.odeyalo.sonata.connect.dto.PlayerStateDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * Call endpoints using WebTestClient
+ */
+public class WebTestClientSonataTestHttpOperations implements SonataTestHttpOperations {
+    private final WebTestClient webTestClient;
+
+    public WebTestClientSonataTestHttpOperations(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
+    }
+
+    @Override
+    public PlayerStateDto getCurrentState(String authorizationHeaderValue) {
+        return webTestClient.get()
+                .uri("/player/currently-playing")
+                .header(HttpHeaders.AUTHORIZATION, authorizationHeaderValue)
+                .exchange().expectBody(PlayerStateDto.class)
+                .returnResult().getResponseBody();
+    }
+
+    @Override
+    public AvailableDevicesResponseDto getConnectedDevices(String authorizationHeaderValue) {
+        return webTestClient.get()
+                .uri("/player/devices")
+                .header(HttpHeaders.AUTHORIZATION, authorizationHeaderValue)
+                .exchange().expectBody(AvailableDevicesResponseDto.class)
+                .returnResult().getResponseBody();
+    }
+
+    @Override
+    public void connectDevice(String authorizationHeaderValue, ConnectDeviceRequest body) {
+        webTestClient.put()
+                .uri("/player/device/connect")
+                .header(HttpHeaders.AUTHORIZATION, authorizationHeaderValue)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(body)
+                .exchange();
+    }
+
+    @Override
+    public void playOrResumePlayback(String authorizationHeaderValue, PlayResumePlaybackRequest body) {
+        webTestClient.put()
+                .uri("/player/play")
+                .header(HttpHeaders.AUTHORIZATION, authorizationHeaderValue)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(body)
+                .exchange();
+    }
+
+    @Override
+    public void changeShuffle(String authorizationHeaderValue, boolean shuffleMode) {
+        webTestClient.put()
+                .uri(builder -> {
+                    builder.queryParam("state", shuffleMode);
+                    return builder.path("/player/shuffle").build();
+                })
+                .header(HttpHeaders.AUTHORIZATION, authorizationHeaderValue)
+                .exchange();
+    }
+}
