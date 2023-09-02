a38e182de41af0cb7e340841fe5c4e204a748519
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-03 00:01:06 +0300

    Base implementation that store the values in Map

---
+++ src/main/java/com/odeyalo/sonata/connect/service/player/sync/InMemoryRoomHolder.java
@@ -0,0 +1,32 @@
+package com.odeyalo.sonata.connect.service.player.sync;
+
+import com.odeyalo.suite.security.auth.AuthenticatedUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static reactor.core.publisher.Mono.fromCallable;
+
+/**
+ * Base implementation that store the values in Map
+ */
+@Component
+public class InMemoryRoomHolder implements RoomHolder {
+    private final Map<String, Room> rooms = new ConcurrentHashMap<>();
+    private final Logger logger = LoggerFactory.getLogger(InMemoryRoomHolder.class);
+
+    @Override
+    public Mono<Room> getOrCreateRoom(AuthenticatedUser user) {
+        Assert.notNull(user, "User cannot be null!");
+        String userId = user.getDetails().getId();
+        return fromCallable(() -> rooms.computeIfAbsent(userId, (key) -> {
+            logger.info("User: {} does not contain ROOM. Creating a new one", user.getDetails().getId());
+            return new Room();
+        }));
+    }
+}
