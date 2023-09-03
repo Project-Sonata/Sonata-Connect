299f9d8d39b2da0da43b4e1fe8deb84f3f298de4
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-03 18:50:10 +0300

    Main interface to synchronize player instance between devices(or clients that can subscribe to events)

---
+++ src/main/java/com/odeyalo/sonata/connect/service/player/sync/PlayerSynchronizationManager.java
@@ -0,0 +1,28 @@
+package com.odeyalo.sonata.connect.service.player.sync;
+
+import com.odeyalo.sonata.connect.service.player.sync.event.PlayerEvent;
+import com.odeyalo.suite.security.auth.AuthenticatedUser;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Main interface to synchronize player instance between devices(or clients that can subscribe to events)
+ *
+ * PlayerSynchronizationManager synchronize player instance for every user by making group of devices and synchronize them all at once
+ */
+public interface PlayerSynchronizationManager {
+    /**
+     * Notify about updated state devices ( or other clients )
+     * @param updatedState - new state to notify the devices
+     * @return - void mono
+     */
+    Mono<Void> publishUpdatedState(AuthenticatedUser user, PlayerEvent updatedState);
+
+    /**
+     * Provide access to subscribe to the events, all events will be published to this stream,
+     * stream can be used to subscribe from WebSocket stream, Text-event-stream, etc.
+     * @param user - user to get event stream
+     * @return - unbound hot-flux that provide real-time updates pushed from {@link #publishUpdatedState(AuthenticatedUser, PlayerEvent)}
+     */
+    Flux<PlayerEvent> getEventStream(AuthenticatedUser user);
+}
