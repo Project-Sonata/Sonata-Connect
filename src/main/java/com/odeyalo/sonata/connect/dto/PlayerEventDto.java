09329aacc9f71987a5a52aa4c7433936d705fbf6
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-03 18:48:27 +0300

    Dto representation of PlayerEvent

---
+++ src/main/java/com/odeyalo/sonata/connect/dto/PlayerEventDto.java
@@ -0,0 +1,33 @@
+package com.odeyalo.sonata.connect.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.odeyalo.sonata.connect.service.player.sync.event.PlayerEvent;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Dto representation of {@link PlayerEvent}
+ */
+@Data
+@NoArgsConstructor
+@SuperBuilder
+@FieldDefaults(level = AccessLevel.PROTECTED)
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "event_type")
+@JsonSubTypes(value = {
+                @JsonSubTypes.Type(value = PlayerStateUpdatedPlayerEventDto.class, name = "PLAYER_STATE_UPDATED"),
+                @JsonSubTypes.Type(value = DeviceConnectedPlayerEventDto.class, name = "NEW_DEVICE_CONNECTED")
+        }
+)
+public abstract class PlayerEventDto {
+
+    @JsonProperty("event_type")
+    public abstract PlayerEvent.EventType getEventType();
+}
