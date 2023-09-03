5988d4412957a42d60d3d01c0437df9258f4309c
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-03 18:46:00 +0300

    Default PlayerSynchronizationManager that just delegate job to RoomEventPublisher

---
+++ src/main/java/com/odeyalo/sonata/connect/service/player/sync/DefaultPlayerSynchronizationManager.java
@@ -0,0 +1,31 @@
+package com.odeyalo.sonata.connect.service.player.sync;
+
+import com.odeyalo.sonata.connect.service.player.sync.event.PlayerEvent;
+import com.odeyalo.suite.security.auth.AuthenticatedUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Default PlayerSynchronizationManager that just delegate job to RoomEventPublisher
+ */
+@Service
+public class DefaultPlayerSynchronizationManager implements PlayerSynchronizationManager {
+    private final RoomHolder roomHolder;
+
+    @Autowired
+    public DefaultPlayerSynchronizationManager(RoomHolder roomHolder) {
+        this.roomHolder = roomHolder;
+    }
+
+    @Override
+    public Mono<Void> publishUpdatedState(AuthenticatedUser user, PlayerEvent event) {
+        return roomHolder.getOrCreateRoom(user).flatMap(room -> room.getPublisher().publishEvent(event));
+    }
+
+    @Override
+    public Flux<PlayerEvent> getEventStream(AuthenticatedUser user) {
+        return roomHolder.getOrCreateRoom(user).flatMapMany(room -> room.getPublisher().getEventStream());
+    }
+}
