f6920bbd09d2d9ffa0224dd358217bf465853a63
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-05 22:31:01 +0300

    Basic tests for device switch endpoint

---
+++ src/test/java/com/odeyalo/sonata/connect/controller/DeviceSwitchPlayerControllerTests.java
@@ -0,0 +1,223 @@
+package com.odeyalo.sonata.connect.controller;
+
+import com.odeyalo.sonata.connect.dto.*;
+import com.odeyalo.sonata.connect.repository.storage.PlayerStateStorage;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Hooks;
+import testing.asserts.AvailableDevicesResponseDtoAssert;
+import testing.asserts.ExceptionMessageAssert;
+import testing.asserts.PlayerStateDtoAssert;
+import testing.faker.ConnectDeviceRequestFaker;
+import testing.shared.SonataTestHttpOperations;
+import testing.spring.autoconfigure.AutoConfigureSonataHttpClient;
+
+import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.REMOTE;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureWebTestClient
+@AutoConfigureSonataHttpClient
+@AutoConfigureStubRunner(stubsMode = REMOTE,
+        repositoryRoot = "git://https://github.com/Project-Sonata/Sonata-Contracts.git",
+        ids = "com.odeyalo.sonata:authorization:+")
+@ActiveProfiles("test")
+public class DeviceSwitchPlayerControllerTests {
+
+    public static final String DEVICE_SWITCH_ENDPOINT = "/player/device/switch";
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    SonataTestHttpOperations sonataTestHttpOperations;
+
+    @Autowired
+    PlayerStateStorage playerStateStorage;
+
+    final String VALID_ACCESS_TOKEN = "Bearer mikunakanoisthebestgirl";
+    final String VALID_USER_ID = "1";
+
+    @BeforeAll
+    void setup() {
+        Hooks.onOperatorDebug(); // DO NOT DELETE IT, VERY IMPORTANT LINE, WITHOUT IT FEIGN WITH WIREMOCK THROWS ILLEGAL STATE EXCEPTION, I DON'T FIND SOLUTION YET
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class NotExistingDeviceRegistryTests {
+
+        @Test
+        void shouldReturnUnprocessableEntityStatus() {
+            WebTestClient.ResponseSpec responseSpec = prepareAndSend();
+
+            responseSpec.expectStatus().isEqualTo(UNPROCESSABLE_ENTITY);
+        }
+
+        @Test
+        void shouldReturnApplicationJson() {
+            WebTestClient.ResponseSpec responseSpec = prepareAndSend();
+
+            responseSpec.expectHeader().contentType(APPLICATION_JSON);
+        }
+
+        @Test
+        void shouldReturnExceptionMessage() {
+            WebTestClient.ResponseSpec responseSpec = prepareAndSend();
+
+            ExceptionMessage message = responseSpec.expectBody(ExceptionMessage.class).returnResult().getResponseBody();
+
+            ExceptionMessageAssert.forMessage(message).isDescriptionEqualTo("Device not found");
+        }
+
+        @NotNull
+        private WebTestClient.ResponseSpec prepareAndSend() {
+            DeviceSwitchRequest body = DeviceSwitchRequest.of(new String[]{"not_existing"});
+
+            return sendRequest(body, VALID_ACCESS_TOKEN);
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class SingleDeviceSwitchTests {
+        String activeDeviceId;
+        String inactiveDeviceId;
+
+        @BeforeEach
+        void prepare() {
+            ConnectDeviceRequest firstDevice = ConnectDeviceRequestFaker.create().get();
+            ConnectDeviceRequest secondDevice = ConnectDeviceRequestFaker.create().get();
+
+            sonataTestHttpOperations.connectDevice(VALID_ACCESS_TOKEN, firstDevice);
+            sonataTestHttpOperations.connectDevice(VALID_ACCESS_TOKEN, secondDevice);
+
+            activeDeviceId = firstDevice.getId();
+            inactiveDeviceId = secondDevice.getId();
+        }
+
+        @AfterEach
+        void clean() {
+            playerStateStorage.clear().block();
+        }
+
+        @Test
+        void shouldReturnNoContentOnSuccess() {
+            WebTestClient.ResponseSpec responseSpec = prepareAndSend();
+
+            responseSpec.expectStatus().isNoContent();
+        }
+
+        @Test
+        void shouldMakeDeviceInactiveToPreviouslyActiveDevice() {
+            WebTestClient.ResponseSpec responseSpec = prepareAndSend();
+
+            AvailableDevicesResponseDto devices = sonataTestHttpOperations.getConnectedDevices(VALID_ACCESS_TOKEN);
+
+            AvailableDevicesResponseDtoAssert.forBody(devices)
+                    .devices().peekById(activeDeviceId).inactive();
+        }
+
+        @Test
+        void shouldMakeDeviceActive() {
+            WebTestClient.ResponseSpec responseSpec = prepareAndSend();
+
+            AvailableDevicesResponseDto devices = sonataTestHttpOperations.getConnectedDevices(VALID_ACCESS_TOKEN);
+
+            AvailableDevicesResponseDtoAssert.forBody(devices)
+                            .devices().peekById(inactiveDeviceId).active(); // Check if the inactive device became active
+        }
+
+        @Test
+        void shouldReturn422StatusCodeIfDeviceIdIsInvalid() {
+            DeviceSwitchRequest body = DeviceSwitchRequest.of(new String[]{"not_existing"});
+
+            WebTestClient.ResponseSpec responseSpec = sendRequest(body, VALID_ACCESS_TOKEN);
+
+            responseSpec.expectStatus().isEqualTo(UNPROCESSABLE_ENTITY);
+        }
+
+        @NotNull
+        private WebTestClient.ResponseSpec prepareAndSend() {
+            DeviceSwitchRequest body = DeviceSwitchRequest.of(new String[]{inactiveDeviceId});
+
+            return sendRequest(body, VALID_ACCESS_TOKEN);
+        }
+    }
+    // TODO: tests are done, implement logic
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class MultipleDeviceIdsInBodyTests {
+
+        @Test
+        void shouldReturn400BadRequestStatusCode() {
+            DeviceSwitchRequest body = DeviceSwitchRequest.of(
+                    new String[]{"something", "ilovemiku", "thirddeviceid"});
+            WebTestClient.ResponseSpec responseSpec = sendRequest(body, VALID_ACCESS_TOKEN);
+
+            responseSpec.expectStatus().isBadRequest();
+        }
+    }
+
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class UnauthorizedRequestTest {
+        String INVALID_ACCESS_TOKEN = "invalidtoken";
+        @Test
+        void shouldReturn401() {
+            WebTestClient.ResponseSpec responseSpec = prepareInvalidAndSend();
+            responseSpec.expectStatus().isUnauthorized();
+        }
+
+        @Test
+        void shouldReturnApplicationJson() {
+            WebTestClient.ResponseSpec responseSpec = prepareInvalidAndSend();
+
+            responseSpec.expectHeader().contentType(APPLICATION_JSON);
+        }
+
+        @Test
+        void shouldReturnExceptionMessage() {
+            WebTestClient.ResponseSpec responseSpec = prepareInvalidAndSend();
+
+            ExceptionMessage body = responseSpec.expectBody(ExceptionMessage.class)
+                    .returnResult().getResponseBody();
+
+            ExceptionMessageAssert.forMessage(body)
+                    .isDescriptionEqualTo("Missing access token or token has been expired");
+        }
+
+        @NotNull
+        private WebTestClient.ResponseSpec prepareInvalidAndSend() {
+            DeviceSwitchRequest request = DeviceSwitchRequest.of(new String[]{"something"});
+
+            return sendRequest(request, INVALID_ACCESS_TOKEN);
+        }
+    }
+
+    @NotNull
+    private WebTestClient.ResponseSpec sendRequest(DeviceSwitchRequest requestBody, @SuppressWarnings("SameParameterValue") String authorizationHeaderValue) {
+        WebTestClient.RequestBodySpec builder = webTestClient.put()
+                .uri(DEVICE_SWITCH_ENDPOINT);
+
+        if (requestBody != null) {
+            builder.bodyValue(requestBody);
+        }
+        builder.header(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE);
+
+        if (authorizationHeaderValue != null) {
+            builder.header(HttpHeaders.AUTHORIZATION, authorizationHeaderValue);
+        }
+
+        return builder.exchange();
+    }
+}
