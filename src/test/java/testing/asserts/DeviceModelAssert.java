94b6458d5f0236d24be5cade5c614db9b1fac5e9
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-05 22:32:23 +0300

    Assert for DeviceModel

---
+++ src/test/java/testing/asserts/DeviceModelAssert.java
@@ -0,0 +1,60 @@
+package testing.asserts;
+
+import com.odeyalo.sonata.connect.model.DeviceModel;
+import com.odeyalo.sonata.connect.model.DeviceType;
+import org.assertj.core.api.AbstractAssert;
+
+import static org.apache.commons.lang.BooleanUtils.isFalse;
+
+public class DeviceModelAssert extends AbstractAssert<DeviceModelAssert, DeviceModel> {
+
+    public DeviceModelAssert(DeviceModel actual) {
+        super(actual, DevicesModelAssert.class);
+    }
+
+    public static DeviceModelAssert forDevice(DeviceModel actual) {
+        return new DeviceModelAssert(actual);
+    }
+
+    public DeviceModelAssert id(String expectedId) {
+        if (!expectedId.equals(actual.getDeviceId())) {
+            failWithActualExpectedAndMessage(actual.getDeviceId(), expectedId, "Expected devices ID to be equal");
+        }
+        return this;
+    }
+
+    public DeviceModelAssert name(String expectedName) {
+        if (!expectedName.equals(actual.getDeviceName())) {
+            failWithActualExpectedAndMessage(actual.getDeviceName(), expectedName, "Expected device names to be equal");
+        }
+        return this;
+    }
+
+    public DeviceModelAssert type(DeviceType expectedType) {
+        if (expectedType != actual.getDeviceType()) {
+            failWithActualExpectedAndMessage(actual.getDeviceType(), expectedType, "Expected device types to be equal");
+        }
+        return this;
+    }
+
+    public DeviceModelAssert volume(int expectedVolume) {
+        if (expectedVolume != actual.getVolume()) {
+            failWithActualExpectedAndMessage(actual.getVolume(), expectedVolume, "Expected device volumes to be equal");
+        }
+        return this;
+    }
+
+    public DeviceModelAssert active() {
+        if (isFalse(actual.isActive())) {
+            failWithMessage("Expected device to be in 'inactive' state!");
+        }
+        return this;
+    }
+
+    public DeviceModelAssert inactive() {
+        if (actual.isActive()) {
+            failWithMessage("Expected device to be in 'inactive' state!");
+        }
+        return this;
+    }
+}
