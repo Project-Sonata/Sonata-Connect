d3f8ad875e8277a4677adfb283690f5d7a9d75e5
odeyalo <theprogrammerinfuture@gmail.com>
2023-09-08 23:04:17 +0300

    Witten InMemoryDevicesBuilder without lombok to generate mutable list as result

--- src/main/java/com/odeyalo/sonata/connect/entity/InMemoryDevices.java
+++ src/main/java/com/odeyalo/sonata/connect/entity/InMemoryDevices.java
@@ -4,6 +4,7 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Data
@@ -12,11 +13,39 @@ import java.util.List;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class InMemoryDevices implements Devices {
-    @Singular
     List<Device> devices;
 
     public static InMemoryDevices empty() {
         return new InMemoryDevices(new ArrayList<>());
+    }
+
+    public static class InMemoryDevicesBuilder {
+        private ArrayList<Device> devices;
+
+        InMemoryDevicesBuilder() {
+        }
+
+        public InMemoryDevicesBuilder device(Device device) {
+            if (this.devices == null) this.devices = new ArrayList<Device>();
+            this.devices.add(device);
+            return this;
+        }
+
+        public InMemoryDevicesBuilder devices(Collection<? extends Device> devices) {
+            if (this.devices == null) this.devices = new ArrayList<Device>();
+            this.devices.addAll(devices);
+            return this;
+        }
+
+        public InMemoryDevicesBuilder clearDevices() {
+            if (this.devices != null)
+                this.devices.clear();
+            return this;
+        }
+
+        public InMemoryDevices build() {
+            return new InMemoryDevices(devices);
+        }
     }
 
     @Override
