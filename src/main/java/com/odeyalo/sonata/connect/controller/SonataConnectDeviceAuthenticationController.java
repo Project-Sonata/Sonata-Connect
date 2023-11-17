a6d22650f43bbd0c3449c64c9286cc90ac84a9cb
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-17 19:57:59 +0200

    Added SCAT exchange for Access Token

--- src/main/java/com/odeyalo/sonata/connect/controller/SonataConnectDeviceAuthenticationController.java
+++ src/main/java/com/odeyalo/sonata/connect/controller/SonataConnectDeviceAuthenticationController.java
@@ -1,21 +1,41 @@
 package com.odeyalo.sonata.connect.controller;
 
+import com.odeyalo.sonata.connect.dto.SCATokenExchangeRequestDto;
+import com.odeyalo.sonata.connect.dto.SCATokenExchangeResponseDto;
 import com.odeyalo.sonata.connect.dto.SonataConnectAuthenticationTokenResponseDto;
+import com.odeyalo.sonata.connect.model.User;
+import com.odeyalo.sonata.connect.service.connect.DeviceConnectionAuthenticationTarget;
+import com.odeyalo.sonata.connect.service.connect.SonataConnectManager;
+import com.odeyalo.suite.security.auth.AuthenticatedUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/connect/auth")
 public class SonataConnectDeviceAuthenticationController {
 
-    @PostMapping
-    public Mono<ResponseEntity<?>> generateSCAT() {
+    private final SonataConnectManager sonataConnectManager;
 
-        return Mono.just(ResponseEntity.ok(
-                SonataConnectAuthenticationTokenResponseDto.of("hello")
-        ));
+    @Autowired
+    public SonataConnectDeviceAuthenticationController(SonataConnectManager sonataConnectManager) {
+        this.sonataConnectManager = sonataConnectManager;
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<SonataConnectAuthenticationTokenResponseDto>> generateSCAT(@RequestParam("target_device_id") String targetDeviceId, AuthenticatedUser user) {
+
+        return sonataConnectManager.generateSCAToken(DeviceConnectionAuthenticationTarget.of(targetDeviceId), User.of(user.getDetails().getId()))
+                .map(scaToken -> SonataConnectAuthenticationTokenResponseDto.of(scaToken.getTokenValue().toString()))
+                .map(ResponseEntity::ok);
+    }
+
+    @PostMapping("/exchange")
+    public Mono<ResponseEntity<SCATokenExchangeResponseDto>> exchangeScatForAccessToken(@RequestBody SCATokenExchangeRequestDto body) {
+        return sonataConnectManager.exchangeForToken(body.getScaToken())
+                .map(accessToken -> SCATokenExchangeResponseDto.of(accessToken.getTokenValue()))
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
     }
 }
--- src/test/java/com/odeyalo/sonata/connect/controller/SonataConnectDeviceAuthenticationEndpointTest.java
+++ src/test/java/com/odeyalo/sonata/connect/controller/SonataConnectDeviceAuthenticationEndpointTest.java
@@ -1,7 +1,11 @@
 package com.odeyalo.sonata.connect.controller;
 
 
+import com.odeyalo.sonata.connect.controller.SonataConnectDeviceAuthenticationEndpointTest.Configuration;
+import com.odeyalo.sonata.connect.dto.ConnectDeviceRequest;
 import com.odeyalo.sonata.connect.dto.SonataConnectAuthenticationTokenResponseDto;
+import com.odeyalo.sonata.connect.service.connect.MockSonataConnectAccessTokenGenerator;
+import com.odeyalo.sonata.connect.service.connect.SonataConnectAccessTokenGenerator;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
@@ -10,26 +14,38 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Hooks;
 import testing.asserts.SonataConnectAuthenticationTokenResponseDtoAssert;
+import testing.faker.ConnectDeviceRequestFaker;
+import testing.shared.SonataTestHttpOperations;
+import testing.spring.autoconfigure.AutoConfigureSonataHttpClient;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.REMOTE;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureWebTestClient
+@AutoConfigureSonataHttpClient
 @AutoConfigureStubRunner(stubsMode = REMOTE,
         repositoryRoot = "git://https://github.com/Project-Sonata/Sonata-Contracts.git",
         ids = "com.odeyalo.sonata:authorization:+")
 @TestPropertySource(locations = "classpath:application-test.properties")
+@Import(Configuration.class)
 public class SonataConnectDeviceAuthenticationEndpointTest {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Autowired
+    SonataTestHttpOperations sonataTestHttpOperations;
 
     final String VALID_ACCESS_TOKEN = "Bearer mikunakanoisthebestgirl";
     final String VALID_USER_ID = "1";
@@ -56,17 +72,45 @@ public class SonataConnectDeviceAuthenticationEndpointTest {
 
             SonataConnectAuthenticationTokenResponseDto responseBody = responseSpec.expectBody(SonataConnectAuthenticationTokenResponseDto.class)
                     .returnResult().getResponseBody();
-
+            System.out.println(responseBody);
             SonataConnectAuthenticationTokenResponseDtoAssert.forResponseBody(responseBody)
                     .hasToken();
         }
 
+        @Test
+        void scatCanBeExchangedForAccessToken() {
+            WebTestClient.ResponseSpec responseSpec = sendValidRequest();
+
+            SonataConnectAuthenticationTokenResponseDto responseBody = responseSpec.expectBody(SonataConnectAuthenticationTokenResponseDto.class)
+                    .returnResult().getResponseBody();
+
+            assertThat(responseBody).isNotNull();
+
+            // HTTP status already asserted in exchangeScat method, won't do it again
+            sonataTestHttpOperations.exchangeScat(VALID_ACCESS_TOKEN, responseBody.getToken());
+        }
+
         @NotNull
         private WebTestClient.ResponseSpec sendValidRequest() {
+
+            ConnectDeviceRequest existingDevice = ConnectDeviceRequestFaker.create().get();
+            sonataTestHttpOperations.connectDevice(VALID_ACCESS_TOKEN, existingDevice);
+
             return webTestClient.post()
-                    .uri("/connect/auth")
+                    .uri((builder) -> builder.path("/connect/auth")
+                            .queryParam("target_device_id", existingDevice.getId())
+                            .build())
                     .header(AUTHORIZATION, VALID_ACCESS_TOKEN)
                     .exchange();
+        }
+    }
+
+    @TestConfiguration
+    static class Configuration {
+
+        @Bean
+        public SonataConnectAccessTokenGenerator sonataConnectAccessTokenGenerator() {
+            return new MockSonataConnectAccessTokenGenerator();
         }
     }
 }
