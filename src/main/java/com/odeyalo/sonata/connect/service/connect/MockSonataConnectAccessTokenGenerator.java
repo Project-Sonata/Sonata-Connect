2970dc25b35be62a3d2bde6dd27e32922be86880
odeyalo <theprogrammerinfuture@gmail.com>
2023-11-17 14:54:42 +0200

    Mock impl of SonataConnectAccessTokenGenerator

---
+++ src/main/java/com/odeyalo/sonata/connect/service/connect/MockSonataConnectAccessTokenGenerator.java
@@ -0,0 +1,17 @@
+package com.odeyalo.sonata.connect.service.connect;
+
+import com.odeyalo.sonata.connect.model.User;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Mock impl of SonataConnectAccessTokenGenerator
+ */
+public class MockSonataConnectAccessTokenGenerator implements SonataConnectAccessTokenGenerator {
+
+    @Override
+    @NotNull
+    public Mono<AccessToken> generateAccessToken(@NotNull User user) {
+        return Mono.just(AccessToken.of("valid_access_token"));
+    }
+}
