708c88b2142afaf18f050556b47a6583bfba4a40
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-31 21:59:51 +0300

    Split shouldUpdateState to 2 separated tests

--- src/test/java/com/odeyalo/sonata/connect/service/player/PlayResumeCommandTests.java
+++ src/test/java/com/odeyalo/sonata/connect/service/player/PlayResumeCommandTests.java
@@ -4,6 +4,7 @@ import com.odeyalo.sonata.connect.entity.PlayerState;
 import com.odeyalo.sonata.connect.exception.ReasonCodeAware;
 import com.odeyalo.sonata.connect.exception.ReasonCodeAwareMalformedContextUriException;
 import com.odeyalo.sonata.connect.model.CurrentPlayerState;
+import com.odeyalo.sonata.connect.model.PlayableItem;
 import com.odeyalo.sonata.connect.model.PlayableItemType;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,6 @@ import reactor.test.StepVerifier;
 
 import static com.odeyalo.sonata.connect.service.player.BasicPlayerOperations.CURRENT_DEVICE;
 import static com.odeyalo.sonata.connect.service.player.DefaultPlayerOperationsTest.DefaultPlayerOperationsTestableBuilder.testableBuilder;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Nested
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -21,17 +21,28 @@ class PlayResumeCommandTests extends DefaultPlayerOperationsTest {
     static final String EXISTING_PLAYABLE_ITEM_CONTEXT = "sonata:track:cassie";
 
     @Test
-    void shouldUpdateState() {
+    void shouldUpdatePlayerStateWithCorrectPlayableItemId() {
         PlayerState playerState = existingPlayerState();
         DefaultPlayerOperations testable = testableBuilder().withState(playerState).build();
 
         testable.playOrResume(DefaultPlayerOperationsTest.EXISTING_USER, PlayCommandContext.of(EXISTING_PLAYABLE_ITEM_CONTEXT), CURRENT_DEVICE)
                 .map(CurrentPlayerState::getPlayableItem)
+                .map(PlayableItem::getId)
                 .as(StepVerifier::create)
-                .assertNext(it -> {
-                    assertThat(it.getId()).isEqualTo("cassie");
-                    assertThat(it.getItemType()).isEqualTo(PlayableItemType.TRACK);
-                })
+                .expectNext("cassie")
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldUpdatePlayerStateWithCorrectPlayableItemType() {
+        PlayerState playerState = existingPlayerState();
+        DefaultPlayerOperations testable = testableBuilder().withState(playerState).build();
+
+        testable.playOrResume(DefaultPlayerOperationsTest.EXISTING_USER, PlayCommandContext.of(EXISTING_PLAYABLE_ITEM_CONTEXT), CURRENT_DEVICE)
+                .map(CurrentPlayerState::getPlayableItem)
+                .map(PlayableItem::getItemType)
+                .as(StepVerifier::create)
+                .expectNext(PlayableItemType.TRACK)
                 .verifyComplete();
     }
 
