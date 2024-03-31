f7967b87b9e0940935de5d4e215bbb203ee469c6
odeyalo <theprogrammerinfuture@gmail.com>
2024-03-31 22:54:39 +0300

    Finished refactoring CurrentPlayerStateTest

--- src/test/java/com/odeyalo/sonata/connect/service/player/CurrentPlayerStateTest.java
+++ src/test/java/com/odeyalo/sonata/connect/service/player/CurrentPlayerStateTest.java
@@ -1,56 +1,18 @@
 package com.odeyalo.sonata.connect.service.player;
 
-import com.odeyalo.sonata.common.context.HardcodedContextUriParser;
-import com.odeyalo.sonata.connect.entity.PlayableItemEntity;
 import com.odeyalo.sonata.connect.entity.PlayerState;
 import com.odeyalo.sonata.connect.entity.UserEntity;
 import com.odeyalo.sonata.connect.model.CurrentPlayerState;
 import com.odeyalo.sonata.connect.model.PlayableItem;
 import com.odeyalo.sonata.connect.model.User;
-import com.odeyalo.sonata.connect.repository.InMemoryPlayerStateRepository;
-import com.odeyalo.sonata.connect.repository.PlayerStateRepository;
-import com.odeyalo.sonata.connect.service.player.handler.PlayerStateUpdatePlayCommandHandlerDelegate;
-import com.odeyalo.sonata.connect.service.player.support.HardcodedPlayableItemResolver;
-import com.odeyalo.sonata.connect.service.player.support.validation.HardcodedPlayCommandPreExecutingIntegrityValidator;
-import com.odeyalo.sonata.connect.service.support.mapper.*;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 import testing.faker.PlayerStateFaker;
-import testing.stub.NullDeviceOperations;
 
-import java.util.Objects;
-import java.util.function.BiConsumer;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static testing.factory.DefaultPlayerOperationsTestableBuilder.testableBuilder;
 
 class CurrentPlayerStateTest {
-
-    public static final User EXISTING_USER = User.of("odeyalooo");
-    PlayerStateRepository playerStateRepository = new InMemoryPlayerStateRepository();
-
-    PlayerState2CurrentPlayerStateConverter converter = new PlayerState2CurrentPlayerStateConverter(
-            new DevicesEntity2DevicesConverter(new DeviceEntity2DeviceConverter()),
-            new PlayableItemEntity2PlayableItemConverter());
-
-    DefaultPlayerOperations playerOperations = new DefaultPlayerOperations(
-            playerStateRepository,
-            new NullDeviceOperations(),
-            converter,
-            new PlayerStateUpdatePlayCommandHandlerDelegate(playerStateRepository, converter,
-                    new HardcodedContextUriParser(),
-                    new HardcodedPlayableItemResolver(),
-                    new HardcodedPlayCommandPreExecutingIntegrityValidator()),
-            new CurrentPlayerState2CurrentlyPlayingPlayerStateConverter());
-
-
-    @AfterEach
-    void afterEach() {
-        playerStateRepository.clear().block();
-    }
+    static final User EXISTING_USER = User.of("odeyalooo");
 
     @Test
     void shouldReturnNotNullExistedState() {
@@ -142,25 +104,35 @@ class CurrentPlayerStateTest {
     }
 
     @Test
-    void shouldReturnCurrentlyPlayingItem() {
-        saveAndCompareActualWithExpected(
-                (expected, actual) -> {
-                    PlayableItemEntity expectedItem = expected.getCurrentlyPlayingItem();
-                    PlayableItem actualItem = actual.getPlayingItem();
-                    assertThat(expectedItem.getId()).isEqualTo(actualItem.getId());
-                    assertThat(expectedItem.getType()).isEqualTo(actualItem.getItemType());
-                }
-        );
+    void shouldReturnCurrentlyPlayingItemId() {
+        PlayerState state = existingPlayerState();
+
+        DefaultPlayerOperations testable = testableBuilder()
+                .withState(state)
+                .build();
+
+        testable.currentState(EXISTING_USER)
+                .map(CurrentPlayerState::getPlayingItem)
+                .map(PlayableItem::getId)
+                .as(StepVerifier::create)
+                .expectNext(state.getCurrentlyPlayingItem().getId())
+                .verifyComplete();
     }
 
-    private void saveAndCompareActualWithExpected(BiConsumer<PlayerState, CurrentPlayerState> predicateConsumer) {
-        PlayerState expected = existingPlayerState();
+    @Test
+    void shouldReturnCurrentlyPlayingItemType() {
+        PlayerState state = existingPlayerState();
 
-        PlayerState playerState = saveState(expected);
+        DefaultPlayerOperations testable = testableBuilder()
+                .withState(state)
+                .build();
 
-        CurrentPlayerState actual = getCurrentPlayerState(playerState);
-
-        predicateConsumer.accept(expected, actual);
+        testable.currentState(EXISTING_USER)
+                .map(CurrentPlayerState::getPlayingItem)
+                .map(PlayableItem::getItemType)
+                .as(StepVerifier::create)
+                .expectNext(state.getCurrentlyPlayingItem().getType())
+                .verifyComplete();
     }
 
     protected static PlayerState existingPlayerState() {
@@ -173,16 +145,4 @@ class CurrentPlayerStateTest {
                 .id(EXISTING_USER.getId())
                 .build();
     }
-
-    @Nullable
-    private CurrentPlayerState getCurrentPlayerState(PlayerState playerState) {
-        return playerOperations.currentState(User.of(playerState.getUser().getId())).block();
-    }
-
-
-    @NotNull
-    protected PlayerState saveState(PlayerState playerState) {
-        return Objects.requireNonNull(playerStateRepository.save(playerState).block());
-    }
-
 }
