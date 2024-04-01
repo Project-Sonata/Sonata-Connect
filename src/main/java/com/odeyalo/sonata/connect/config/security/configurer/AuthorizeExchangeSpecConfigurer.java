e3895c173fda599abf3ad4399ce5729368716835
odeyalo <theprogrammerinfuture@gmail.com>
2024-04-01 20:40:51 +0300

    Removed mocked beans AuthorizeExchangeSpecConfigurer

--- src/main/java/com/odeyalo/sonata/connect/config/security/configurer/AuthorizeExchangeSpecConfigurer.java
+++ src/main/java/com/odeyalo/sonata/connect/config/security/configurer/AuthorizeExchangeSpecConfigurer.java
@@ -1,18 +1,8 @@
 package com.odeyalo.sonata.connect.config.security.configurer;
 
-import com.odeyalo.suite.security.auth.TokenAuthenticationManager;
-import com.odeyalo.suite.security.auth.token.AccessTokenMetadata;
-import com.odeyalo.suite.security.auth.token.ValidatedAccessToken;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
-
-import java.time.Instant;
-import java.util.Objects;
 
 /**
  * Helper class to configure the {@link AuthorizeExchangeSpec}
@@ -26,42 +16,5 @@ public class AuthorizeExchangeSpecConfigurer implements Customizer<AuthorizeExch
         authorizeExchangeSpec
                 .pathMatchers(SCA_TOKEN_EXCHANGE_ENDPOINT).permitAll()
                 .anyExchange().authenticated();
-    }
-
-
-    @Bean
-    @Primary
-    public ReactiveAuthenticationManager reactiveAuthenticationManager() {
-        return new TokenAuthenticationManager(
-                tokenValue -> {
-                    if ( Objects.equals(tokenValue, "token1") ) {
-                        return Mono.just(
-                                ValidatedAccessToken.valid(
-                                        AccessTokenMetadata.of("123", new String[]{"read", "write"},
-                                                Instant.now().getEpochSecond(),
-                                                Instant.now().plusSeconds(300).getEpochSecond()
-                                        ))
-                        );
-                    }
-                    if ( Objects.equals(tokenValue, "token1_2") ) {
-                        return Mono.just(ValidatedAccessToken.valid(
-                                AccessTokenMetadata.of("123", new String[]{"read", "write", "playlist"},
-                                        Instant.now().getEpochSecond(),
-                                        Instant.now().plusSeconds(300).getEpochSecond()
-                                )));
-                    }
-
-                    if ( Objects.equals(tokenValue, "token2_2") ) {
-                        return Mono.just(
-                                ValidatedAccessToken.valid(
-                                        AccessTokenMetadata.of("miku", new String[]{"read", "write", "playlist"},
-                                                Instant.now().getEpochSecond(),
-                                                Instant.now().plusSeconds(300).getEpochSecond()
-                                        )));
-                    }
-
-                    return Mono.just(ValidatedAccessToken.invalid());
-                }
-        );
     }
 }
