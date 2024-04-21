a251ea4bf7abb892bfe977e4955122799147642d
odeyalo <theprogrammerinfuture@gmail.com>
2024-04-21 17:25:40 +0300

    Added meta annotations and docs for PlayableItemEntity and impementations

--- src/main/java/com/odeyalo/sonata/connect/entity/PlayableItemEntity.java
+++ src/main/java/com/odeyalo/sonata/connect/entity/PlayableItemEntity.java
@@ -1,9 +1,15 @@
 package com.odeyalo.sonata.connect.entity;
 
 import com.odeyalo.sonata.connect.model.PlayableItemType;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represent an entity of the item that can be played by a user.
+ */
 public interface PlayableItemEntity {
+    @NotNull
     String getId();
 
+    @NotNull
     PlayableItemType getType();
 }
--- src/main/java/com/odeyalo/sonata/connect/entity/TrackItemEntity.java
+++ src/main/java/com/odeyalo/sonata/connect/entity/TrackItemEntity.java
@@ -3,21 +3,28 @@ package com.odeyalo.sonata.connect.entity;
 import com.odeyalo.sonata.connect.model.PlayableItemType;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represent a track entity that can be player by a user
+ */
 @Data
 @AllArgsConstructor(staticName = "of")
 @NoArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TrackItemEntity implements PlayableItemEntity {
+    @NotNull
     String id;
 
     @Override
+    @NotNull
     public String getId() {
         return id;
     }
 
     @Override
+    @NotNull
     public PlayableItemType getType() {
         return PlayableItemType.TRACK;
     }
