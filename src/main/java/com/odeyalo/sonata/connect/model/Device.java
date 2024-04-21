7afb296093d695c2c2ff92356d3dbdbf5e2134ac
odeyalo <theprogrammerinfuture@gmail.com>
2024-04-21 17:15:32 +0300

    Added NotNull annotations for Device POJO

--- src/main/java/com/odeyalo/sonata/connect/model/Device.java
+++ src/main/java/com/odeyalo/sonata/connect/model/Device.java
@@ -3,13 +3,21 @@ package com.odeyalo.sonata.connect.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represent a device of the user where a playback can be started
+ * or from which playback state can be changed.
+ */
 @Value
 @AllArgsConstructor(staticName = "of")
 @Builder(toBuilder = true)
 public class Device {
+    @NotNull
     String deviceId;
+    @NotNull
     String deviceName;
+    @NotNull
     DeviceType deviceType;
     int volume;
     boolean active;
