b0513873d9d5042dfc15c8f0046f7cc53373b20f
odeyalo <theprogrammerinfuture@gmail.com>
2024-06-17 20:39:06 +0300

    Replaced manual building of object with Mapstruct

--- src/main/java/com/odeyalo/sonata/connect/service/support/mapper/dto/PlayableItem2PlayableItemDtoConverter.java
+++ src/main/java/com/odeyalo/sonata/connect/service/support/mapper/dto/PlayableItem2PlayableItemDtoConverter.java
@@ -5,27 +5,40 @@ import com.odeyalo.sonata.connect.dto.TrackItemDto;
 import com.odeyalo.sonata.connect.model.PlayableItem;
 import com.odeyalo.sonata.connect.model.TrackItemSpec;
 import com.odeyalo.sonata.connect.service.support.mapper.Converter;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PlayableItem2PlayableItemDtoConverter implements Converter<PlayableItem, PlayableItemDto> {
+    private final TrackItemSpec2TrackItemDtoConverter trackConverter;
+
+    public PlayableItem2PlayableItemDtoConverter(final TrackItemSpec2TrackItemDtoConverter trackConverter) {
+        this.trackConverter = trackConverter;
+    }
 
     @Override
     public PlayableItemDto convertTo(PlayableItem playableItem) {
+
         if ( playableItem == null ) {
             return null;
         }
-        if ( (playableItem instanceof TrackItemSpec trackSpec) ) {
-            return TrackItemDto.builder()
-                    .id(trackSpec.getId())
-                    .name(trackSpec.getName())
-                    .durationMs(trackSpec.getDuration().asMilliseconds())
-                    .contextUri(trackSpec.getContextUri().asString())
-                    .explicit(trackSpec.isExplicit())
-                    .discNumber(trackSpec.getOrder().discNumber())
-                    .index(trackSpec.getOrder().index())
-                    .build();
+
+        if ( playableItem instanceof TrackItemSpec trackSpec ) {
+            return trackConverter.toTrackItemDto(trackSpec);
         }
+
         throw new UnsupportedOperationException(String.format("PlayableItem2PlayableItemDtoConverter does not support: %s", playableItem.getItemType()));
+    }
+
+    @Mapper(componentModel = "spring")
+    public interface TrackItemSpec2TrackItemDtoConverter {
+
+        @Mapping(target = "discNumber", expression = "java( spec.getOrder().discNumber() )")
+        @Mapping(target = "index", expression = "java( spec.getOrder().index() )")
+        @Mapping(target = "contextUri", expression = "java( spec.getContextUri().asString() )")
+        @Mapping(target = "durationMs", expression = "java( spec.getDuration().asMilliseconds() )")
+        TrackItemDto toTrackItemDto(TrackItemSpec spec);
+
     }
 }
