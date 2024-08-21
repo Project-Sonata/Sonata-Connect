60f2e1e6b590bec1323dd73b575ce07ff97f69c7
odeyalo <theprogrammerinfuture@gmail.com>
2024-08-21 20:39:58 +0300

    AuthorizeExchangeSpecConfigurer now allows to send HTTP OPTIONS request for any user(even anonymous one)

--- src/main/java/com/odeyalo/sonata/connect/config/security/configurer/AuthorizeExchangeSpecConfigurer.java
+++ src/main/java/com/odeyalo/sonata/connect/config/security/configurer/AuthorizeExchangeSpecConfigurer.java
@@ -1,5 +1,6 @@
 package com.odeyalo.sonata.connect.config.security.configurer;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec;
 import org.springframework.stereotype.Component;
@@ -8,13 +9,14 @@ import org.springframework.stereotype.Component;
  * Helper class to configure the {@link AuthorizeExchangeSpec}
  */
 @Component
-public class AuthorizeExchangeSpecConfigurer implements Customizer<AuthorizeExchangeSpec> {
+public final class AuthorizeExchangeSpecConfigurer implements Customizer<AuthorizeExchangeSpec> {
     private static final String SCA_TOKEN_EXCHANGE_ENDPOINT = "/connect/auth/exchange**";
 
     @Override
     public void customize(AuthorizeExchangeSpec authorizeExchangeSpec) {
         authorizeExchangeSpec
                 .pathMatchers(SCA_TOKEN_EXCHANGE_ENDPOINT).permitAll()
+                .pathMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyExchange().authenticated();
     }
 }
