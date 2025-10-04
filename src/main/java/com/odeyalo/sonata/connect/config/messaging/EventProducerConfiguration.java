41a737c7b3abe479bba8e964655fe27ef17d1004
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-04 14:05:21 +0300

    create SpyMessageSendingTemplate for tests

--- src/main/java/com/odeyalo/sonata/connect/config/messaging/EventProducerConfiguration.java
+++ src/main/java/com/odeyalo/sonata/connect/config/messaging/EventProducerConfiguration.java
@@ -1,12 +1,14 @@
 package com.odeyalo.sonata.connect.config.messaging;
 
 import com.odeyalo.sonata.connect.service.messaging.MessageSendingTemplate;
+import com.odeyalo.sonata.connect.service.messaging.SpyMessageSendingTemplate;
 import com.odeyalo.sonata.connect.service.messaging.kafka.KafkaMessageSendingTemplate;
 import com.odeyalo.sonata.suite.brokers.events.SonataEvent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import reactor.kafka.sender.KafkaSender;
 
 @Configuration
@@ -14,8 +16,14 @@ public class EventProducerConfiguration {
 
     @Bean
     @ConditionalOnBean(KafkaSender.class)
+    @Profile("!test")
     public MessageSendingTemplate<String, SonataEvent> messageSendingTemplate(@NotNull final KafkaSender<String, SonataEvent> sender) {
         return new KafkaMessageSendingTemplate<>(sender);
     }
 
+    @Bean
+    @Profile("test")
+    public SpyMessageSendingTemplate<String, SonataEvent> messageSendingTemplate() {
+        return new SpyMessageSendingTemplate<>();
+    }
 }
